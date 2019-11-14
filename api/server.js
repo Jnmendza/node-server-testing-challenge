2cee26103ae6f0374ad29d8e0edbee38c6d0e03a
Jnmendza <jonatmendoza@gmail.com>
2019-11-14 12:32:11 -0800

    server set up

---
+++ api/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+// const Players = require('../players/playersModel.js')
+
+const server = express();
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.status(200).json({ Player: 'ready' })
+});
+
+server.get('/players', (req, res) => {
+    // Players.getAll()
+    // .then(players => {
+    //     res.status(200).json(players);
+    // })
+    // .catch(error => {
+    //     res.status(500).json(erros)
+    // })
+})
+
+module.exports = server;
